805ba49837b3721071d37df27f06d97a02356681
Ada <larmartinez2@gmail.com>
2018-12-11 15:18:01 -0800

    set up state

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
 import './PlayerSubmissionForm.css';
+import PropTypes from 'prop-types';
 
 class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     console.log(props.data)
     super(props);
+    this.state = {
+      adj1: '',
+      noun1: '',
+      adv: '',
+      verb: '',
+      adj2: '',
+      noun2: '',
+    };
   }
 
-  onInputChange = (event) => {
-    const updatedState = {};
+  onFieldChangeHandler = (event, fieldToGetValueFrom = 'value') => {
+    const updateState = {};
 
-    const field = event.target.name;
-    const value = event.target.value;
+    const fieldName = event.target.name;
+    const value = event.target[fieldToGetValueFrom];
 
-    updatedState[field] = value;
-    this.setState(updatedState);
+    updateState[fieldName] = value;
+
+    this.setState(updateState);
   }
+
+  onSubmitHandler = (event)=> {
+    event.preventDefault();
+      this.props.addNewLineCallback(this.state);
+      this.setState({
+        adj1: '',
+        noun1: '',
+        adv: '',
+        verb: '',
+        adj2: '',
+        noun2: '',
+      });
+  }
+
   render() {
 
     return (
@@ -26,26 +50,43 @@ class PlayerSubmissionForm extends Component {
         <form className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-          {
-
-          }
             <span>The</span>
             <input
+              name="adj1"
               placeholder="adjective"
+              value = {this.state.adj1}
+              onChange={this.onFieldChangeHandler}
               type="text" />
-            <input placeholder="noun" type="text" />
             <input
+              name="noun1"
+              placeholder="noun"
+              value = {this.state.noun1}
+              onChange={this.onFieldChangeHandler}
+              type="text" />
+            <input
+              name="adv"
               placeholder="adverb"
+              value = {this.state.adv}
+              onChange={this.onFieldChangeHandler}
               type="text" />
             <input
+              name="verb"
               placeholder="verb"
+              value = {this.state.verb}
+              onChange={this.onFieldChangeHandler}
               type="text" />
             <span>the</span>
             <input
+              name="adj2"
               placeholder="adjective"
+              value = {this.state.adj2}
+              onChange={this.onFieldChangeHandler}
               type="text" />
             <input
+              name="noun2"
               placeholder="noun"
+              value = {this.state.noun2}
+              onChange={this.onFieldChangeHandler}
               type="text" />
             <span>.</span>
           </div>
@@ -58,5 +99,10 @@ class PlayerSubmissionForm extends Component {
     );
   }
 }
+
+
+PlayerSubmissionForm.propTypes = {
+  addNewLineCallback: PropTypes.func.isRequired,
+};
 
 export default PlayerSubmissionForm;
